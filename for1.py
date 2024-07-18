0d259a1c46ea190ad7f3d980d422f183da822116
AshleshaAjankar <ashu.ajans98@gmail.com>
2024-07-18 13:02:28 +0530

    python pac commited

---
+++ for1.py
@@ -0,0 +1,22 @@
+x= ['ash', 12, 2.6]
+
+for i in x:
+    print (i)
+
+    #to print value one by one
+
+
+  for i in [2, 8, 'ashu']:
+   print (i)
+
+
+#to print value in accending order
+#0-9 will print 
+
+for i in range(10):
+    print(i)
+
+
+#to print value from 11-20 nad diff will be 1
+  for i in range(11,21,1):
+     print(i)
---
+++ for2.py
@@ -0,0 +1,15 @@
+for i in range(11,21):
+    print(i)
+
+for i in range(21,31,2):
+    print(i)
+
+#for decending order
+for i in range(20,11,-1):
+    print(i)
+    
+#value divisible by 5
+    for i in range(1,20):
+        if i%5!=0:
+            print(i)
+            
